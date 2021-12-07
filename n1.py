e20aefe4248be84bd186faed35ba595bfa928c9d
el_brayan <elvis.llampasi.27@unsch.edu.pe>
2021-12-06 22:28:39 -0500

    (palabra)

---
+++ n1.py
@@ -0,0 +1,13 @@
+def palindromo(frase):
+    frase = frase.lower()
+    frase = frase.replace(' ','')
+    if frase == frase[::-1]:
+        print('es palindromo')
+        print(frase)
+    else:
+        print('no es palindromo')
+        print(frase)
+
+while True:
+    frase =input('ingrese una frase o palabra: ')
+    palindromo(frase)
